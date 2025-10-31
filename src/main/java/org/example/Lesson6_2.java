19712c44b872fc88e9472c97ea132cadbb1de9de
AnnNep <a_nepogodina@bk.ru>
2025-10-31 16:35:05 +0500

    learn do+while, while, for, for+if+continue/break

---
+++ src/main/java/org/example/Lesson6_2.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Lesson6_2 {
+    static void main() {
+        for (int value = 10; value > 0; value--) {
+            if (value == 5) {
+                continue;
+//                break;
+            }
+            System.out.println(value);
+        }
+    }
+
+
+//    private static void forExample(); {
+//        int i = 0;
+//        for (; ; ) {
+//            System.out.println(i);
+//        }
+//         for (int value = 10; value > 0; value--){
+//            System.out.println(value);
+//        }
+        }
