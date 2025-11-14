914c15fbb5c6152c0a60d77a7d4ef754b88ed6e2
AnnNep <a_nepogodina@bk.ru>
2025-11-14 16:24:22 +0500

    Inheritance WITH THEORY

--- src/main/java/org/example/Lesson5_1/Laptop.java
+++ src/main/java/org/example/Lesson5_1/Laptop.java
@@ -1,17 +1,17 @@
 package org.example.Lesson5_1;
 
 public class Laptop extends Computer {
-
+//Наследование - объект одного класса наследует поля/функционал от другого
     private int weight;
-
+//В самой первой строчке конструктора-наследника обязан быть вызван конструктор баз класса через супер
     public Laptop(Ssd ssd, Ram ram, int weight) {
-        super(ssd, ram);
+        super(ssd, ram); //через super можно достучаться до всего, что доступно в базовом классе
         this.weight = weight;
-        super.getRam();
+        super.getRam(); //через геттеры можно достучатья до private полей
         super.getSsd();
         getWeight();
     }
-
+//В java можно наследовать только 1 класс, в с++ много
     public void open() {
         System.out.println("Opening laptop");
     }
