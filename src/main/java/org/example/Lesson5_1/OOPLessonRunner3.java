8d8e0f4e0e4b157b8f3bb5188ba809ccd5b29e9f
AnnNep <a_nepogodina@bk.ru>
2025-11-16 00:07:42 +0500

    Type conversion. The instanceof operator WITH THEORY

--- src/main/java/org/example/Lesson5_1/OOPLessonRunner3.java
+++ src/main/java/org/example/Lesson5_1/OOPLessonRunner3.java
@@ -6,13 +6,21 @@ public class OOPLessonRunner3 {
         Computer laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Computer mobile = new Mobile(new Ssd(125), new Ram(512));
 
-        loadComputers(laptop, mobile);
-        printInformation(new Computer[]{laptop, mobile});
+        Laptop laptop1 = new Laptop(new Ssd(5000), new Ram(10000), 5);
+
+//        loadComputers(laptop, mobile);
+        printInformation(new Computer[]{laptop, mobile, laptop1});
     }
 
     public static void printInformation (Computer[] computers) {
         for (Computer computer : computers) {
             computer.print();
+            //instanceof проверяет, действительно ли ссылка Computer ведет на объект типа laptop
+            if (computer instanceof Laptop) {
+                Laptop laptop = (Laptop) computer;
+                laptop.open();
+            }
+            System.out.println();
         }
     }
 
