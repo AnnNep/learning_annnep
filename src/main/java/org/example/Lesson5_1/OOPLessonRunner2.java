9582ac5445df23b0b83a2e4227941931a5f58f44
AnnNep <a_nepogodina@bk.ru>
2025-11-16 22:08:00 +0500

    object initialization Edited

--- src/main/java/org/example/Lesson5_1/OOPLessonRunner2.java
+++ src/main/java/org/example/Lesson5_1/OOPLessonRunner2.java
@@ -5,6 +5,17 @@ public class OOPLessonRunner2 {
     public static void main(String[] args) {
         //Laptop -> Computer -> Object
         //Laptop <- Computer <- Object
+
+//        Computer comp = new Laptop()
+//        comp. <- здесь доступны методы только класса Computer, потому что типом ссылки является Computer
+//
+//        Laptop comp = new Laptop()
+//        comp. <- доступны методы Computer & Laptop, т. к. Laptop является наследником Computer и класс Laptop добавляет свой функционал
+//
+//        Object obj = new Computer()/ new Laptop()
+//        obj. <- доступны методы только класса Object (базовый класс для всех классов в java) (toString можешь пока заюзать)
+
+
         new Laptop();
     }
 }
