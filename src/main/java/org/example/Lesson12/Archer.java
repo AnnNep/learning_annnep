b6322c5a38e5e2918f384e93408eaac0504bde3a
AnnNep <a_nepogodina@bk.ru>
2025-11-18 19:38:55 +0500

    inner and nested classes WITH THEORY

--- src/main/java/org/example/Lesson12/Archer.java
+++ src/main/java/org/example/Lesson12/Archer.java
@@ -2,13 +2,39 @@ package org.example.Lesson12;
 
 public class Archer extends Hero {
 
+    private Wolf wolf;
+
     public Archer(String name, int damage) {
         super(name, damage);
+        this.wolf = new Wolf("Wolf", 7);
     }
 
     @Override
     public void attackEnemy(Enemy enem) {
-        System.out.println(getName()+ " стрельнул из лука в " + enem.getName());
-        enem.takeDamage(getDamage());
+        //Локальный класс - класс, объявляемый внутри метода (не используется на практике, но нужно знать, что такой есть)
+        class Test {
+
+        }
+
+        Test t = new Test();
+        t.toString();
+
+        System.out.println(getName() + " стрельнул из лука в " + enem.getName());
+        wolf.attackEnemy(enem);
+    }
+    public static class Wolf{ //Без static - внутренний класс, с static - вложенный
+        private String name;
+        private int damage;
+
+        public Wolf(String name, int damage) {
+
+          this.name = name;
+          this.damage = damage;
+    }
+
+    public void attackEnemy(Enemy enemy) {
+        System.out.println(name+ " и " + Archer.this.getName() + " наносят совместный урон");
+        enemy.takeDamage(damage + Archer.this.getDamage()); //Обращение к объекту внешнего класса
+        }
     }
 }
--- src/main/java/org/example/Lesson12/TrainingGround.java
+++ src/main/java/org/example/Lesson12/TrainingGround.java
@@ -5,8 +5,11 @@ public class TrainingGround {
     public static void main(String[] args) {
         Hero war = new Warrior("Боромир", 15);
         Hero mag = new Mage("Гэндольф", 20);
-        Hero arc = new Archer("Леголас", 10);
+        Archer arc = new Archer("Леголас", 10);
 
+//        Archer.Wolf wolf = arc.new Wolf("Second Wolf", 11); //Мы создаем объект внутреннего класса, используя ссылку внешнего класса
+        Archer.Wolf wolf = new Archer.Wolf("Second Wolf", 11); //А для вложенных классов наоборот: не нужно использовать ссылку внешнего класса, взамен пишем полный путь к классу Wolf
+//Можно сделать импорт класса арчер, чтобы не ссылаться на него
         Enemy enem = new Enemy("Зомби", 100);
 
         attackEnemy(enem,war,mag,arc);
