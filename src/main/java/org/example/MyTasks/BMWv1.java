b3e11f3438a76a228965a79c53d65fd663b36b4f
AnnNep <a_nepogodina@bk.ru>
2025-11-23 00:45:40 +0500

    My task1

--- src/main/java/org/example/MyTasks/BMWv1.java
+++ src/main/java/org/example/MyTasks/BMWv1.java
@@ -1,7 +1,7 @@
 package org.example.MyTasks;
 
 public final class BMWv1 extends BMW {
-
+    //От класса БМВв1 уже не будут создаваться наследники, так как это конкретный, не абстрактный объект
     private String sound;
 
     public BMWv1(int howOftenTO, String country, int year, String sound) {
--- src/main/java/org/example/MyTasks/CarRunner.java
+++ src/main/java/org/example/MyTasks/CarRunner.java
@@ -4,8 +4,8 @@ public class CarRunner {
     static void main(String[] args) {
         Car car = new BMWv1 (11,"Германия", 2015, "cool sound");
         System.out.println(car.ride());
-        Car car2 = new BMW(10,"Германия",2020);
-        BMW bmw = new BMW(7,"Германия",2017);
+        Car car2 = new BMW(10,"Германия",2020); //Созданный объект имеет функции только базового класса Кар
+        BMW bmw = new BMW(7,"Германия",2017); //А у этого объекта добавляется функционал наследника БМВ
         Toyota toyota = new Toyota(15, "Япония", 2013);
         toyota.CarAdvantages();
         toyota.CarInformation();
