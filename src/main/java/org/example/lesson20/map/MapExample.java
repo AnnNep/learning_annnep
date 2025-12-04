96c262dd21b0cad31498bb32c131bd2b94df136a
AnnNep <a_nepogodina@bk.ru>
2025-12-05 00:37:41 +0500

    HashMap 2 WITH THEORY

--- src/main/java/org/example/lesson20/map/MapExample.java
+++ src/main/java/org/example/lesson20/map/MapExample.java
@@ -17,6 +17,29 @@ public class MapExample {
         map.put(petr.getId(),petr);
 
         System.out.println(map.get(2));
+
+        //Map не является коллекцией, но содержить их:
+        System.out.println(map.keySet()); //набор всех ключей
+        System.out.println(map.values()); //коллекция значений
+        System.out.println(map.entrySet()); //множество всех ключей и значений пар(объект типа Node<K,V> реализует интерфейс Entry<K,V>
+    //Мы можем итерироваться по любым из этих коллекций
+        for (Person person : map.values()) {
+            System.out.println(person.getName());
+        }
+
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+
+        System.out.println(map.containsKey(1)); //есть ли в мап нод с таким ключем
+        System.out.println(map.size()); //сколько ключей мап
+        System.out.println(map.isEmpty()); //пустая ли map
+        System.out.println(map.putIfAbsent(1,new Person(1, "Ivan", "Ivanov"))); //если такой ключ существует, то не класть новое значение
+        System.out.println(map.getOrDefault(5,new Person(1, "Ivan", "Ivanov")));//если под 5 индексом ничего нет, то пусть будет дефолтная строчка
+        //load factor - создание нового массива происходит не когда старый размер полностью заполнен, а когда заполнен какой то его процент, например 70%
+        //при увеличении массива нужно перераспределить все ноды, так как индексы нодов снова нужно пересчитывать
+        //Map не поддерживает повторение ключей, он должен быть уникальным, иначе одно значение перетирается другим
+
         //Поле table - массив
         //Класс node - каждая ячейка
         //HashMap - основан на хэш таблицах, а это просто массив из 16 ячеек по умолчанию
